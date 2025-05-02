fb8670a6a572c4790db9aa9963fb6db4bdd828d4
Shengyuan Jia <jia_shengyuan@163.com>
2025-05-02 22:36:45 +0800

    更新ui

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, reactive } from 'vue'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 
 // 配置信息
 const formData = reactive({
@@ -17,21 +17,31 @@ const formData = reactive({
 const courses = ref([
   {
     name: '高等数学',
-    teacher: '张老师',
-    time: '周一 1-2节',
-    location: '理教101'
+    classes: [
+      {
+        id: 1,
+        teacher: '张老师',
+        time: '周一 1-2节',
+        location: '理教101'
+      },
+      {
+        id: 2,
+        teacher: '李老师',
+        time: '周二 3-4节',
+        location: '理教102'
+      }
+    ]
   },
   {
     name: '大学物理',
-    teacher: '李老师',
-    time: '周三 3-4节',
-    location: '理教201'
-  },
-  {
-    name: '计算机导论',
-    teacher: '王老师',
-    time: '周五 5-6节',
-    location: '理教301'
+    classes: [
+      {
+        id: 3,
+        teacher: '王老师',
+        time: '周三 3-4节',
+        location: '理教201'
+      }
+    ]
   }
 ])
 
@@ -39,66 +49,164 @@ const courses = ref([
 const addCourseDialogVisible = ref(false)
 const newCourse = reactive({
   name: '',
-  teacher: '',
-  time: '',
-  location: ''
+  classes: [{
+    id: '',
+    teacher: '',
+    time: '',
+    location: ''
+  }]
 })
 
 // 编辑课程对话框
 const editCourseDialogVisible = ref(false)
 const editingCourseIndex = ref(-1)
+const editingClassIndex = ref(-1)
 const editingCourse = reactive({
   name: '',
-  teacher: '',
-  time: '',
-  location: ''
+  classes: [{
+    id: '',
+    teacher: '',
+    time: '',
+    location: ''
+  }]
 })
+
+const activeNames = ref([]) // 控制折叠面板的展开状态
+const coursePreference = ref('') // 选课倾向
+const minCredits = ref(0) // 最少学分
+const maxCredits = ref(0) // 最多学分
+
+// 处理最低学分变化
+const handleMinCreditsChange = (value) => {
+  if (value > maxCredits.value) {
+    maxCredits.value = value
+  }
+}
+
+// 处理最高学分变化
+const handleMaxCreditsChange = (value) => {
+  if (value < minCredits.value) {
+    minCredits.value = Math.max(0, value)
+  }
+}
 
 // 显示添加课程对话框
 const showAddCourseDialog = () => {
   addCourseDialogVisible.value = true
   // 清空表单
-  Object.keys(newCourse).forEach(key => {
-    newCourse[key] = ''
-  })
+  newCourse.name = ''
+  newCourse.classes = [{
+    id: '',
+    teacher: '',
+    time: '',
+    location: ''
+  }]
 }
 
 // 显示编辑课程对话框
-const showEditCourseDialog = (index) => {
-  editingCourseIndex.value = index
-  const course = courses.value[index]
+const showEditCourseDialog = (courseIndex, classIndex) => {
+  editingCourseIndex.value = courseIndex
+  editingClassIndex.value = classIndex
+  const course = courses.value[courseIndex]
+  const classInfo = course.classes[classIndex]
   editingCourse.name = course.name
-  editingCourse.teacher = course.teacher
-  editingCourse.time = course.time
-  editingCourse.location = course.location
+  editingCourse.classes = [{
+    id: classInfo.id,
+    teacher: classInfo.teacher,
+    time: classInfo.time,
+    location: classInfo.location
+  }]
   editCourseDialogVisible.value = true
 }
 
-// 添加课程
+// 添加课程（如"数学分析"）
 const addCourse = () => {
   if (!newCourse.name) {
     ElMessage.warning('请输入课程名称')
     return
   }
-  courses.value.push({ ...newCourse })
+  // 自动添加一个样例班级
+  const newId = Math.max(...courses.value.flatMap(c => c.classes.map(cl => cl.id)), 0) + 1
+  courses.value.push({
+    name: newCourse.name,
+    classes: [{
+      id: newId,
+      teacher: '待设置',
+      time: '待设置',
+      location: '待设置'
+    }]
+  })
   addCourseDialogVisible.value = false
   ElMessage.success('添加成功')
 }
 
-// 编辑课程
-const editCourse = () => {
-  if (!editingCourse.name) {
-    ElMessage.warning('请输入课程名称')
+// 添加班级（如"高等数学 王老师班"）
+const addClass = (courseIndex) => {
+  const course = courses.value[courseIndex]
+  const newId = Math.max(...course.classes.map(c => c.id), 0) + 1
+  // 打开编辑对话框
+  editingCourseIndex.value = courseIndex
+  editingClassIndex.value = course.classes.length
+  editingCourse.name = course.name
+  editingCourse.classes = [{
+    id: newId,
+    teacher: '',
+    time: '',
+    location: ''
+  }]
+  editCourseDialogVisible.value = true
+}
+
+// 编辑班级
+const editClass = () => {
+  if (!editingCourse.name || !editingCourse.classes[0].id) {
+    ElMessage.warning('请输入课程编号和教师信息')
     return
   }
-  courses.value[editingCourseIndex.value] = { ...editingCourse }
+  const courseIndex = editingCourseIndex.value
+  const classIndex = editingClassIndex.value
+  const course = courses.value[courseIndex]
+  
+  // 如果是新添加的班级
+  if (classIndex === course.classes.length) {
+    course.classes.push({ ...editingCourse.classes[0] })
+    // 自动展开该课程
+    if (!activeNames.value.includes(courseIndex)) {
+      activeNames.value.push(courseIndex)
+    }
+  } else {
+    course.classes[classIndex] = { ...editingCourse.classes[0] }
+  }
+  
   editCourseDialogVisible.value = false
   ElMessage.success('编辑成功')
 }
 
 // 删除课程
-const deleteCourse = (index) => {
-  courses.value.splice(index, 1)
+const deleteCourse = (courseIndex) => {
+  ElMessageBox.confirm(
+    `确定要删除课程"${courses.value[courseIndex].name}"及其所有班级吗？`,
+    '删除确认',
+    {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    }
+  ).then(() => {
+    courses.value.splice(courseIndex, 1)
+    ElMessage.success('删除成功')
+  }).catch(() => {
+    // 用户取消删除，不做任何操作
+  })
+}
+
+// 删除班级
+const deleteClass = (courseIndex, classIndex) => {
+  const course = courses.value[courseIndex]
+  course.classes.splice(classIndex, 1)
+  if (course.classes.length === 0) {
+    courses.value.splice(courseIndex, 1)
+  }
   ElMessage.success('删除成功')
 }
 
@@ -118,15 +226,23 @@ const handleConfirm = () => {
 
 // 处理文件导入
 const handleFileImport = (file) => {
-  // 暂时只使用文件名作为课程名
-  const courseName = file.name.split('.')[0] // 去掉文件扩展名
+  const courseName = file.name.split('.')[0]
+  const newId = courses.value.length + 1
   courses.value.push({
     name: courseName,
-    teacher: '待设置',
-    time: '待设置',
-    location: '待设置'
+    classes: [{
+      id: newId,
+      teacher: '待设置',
+      time: '待设置',
+      location: '待设置'
+    }]
   })
   ElMessage.success(`成功导入培养方案：${courseName}`)
+}
+
+// 保存选课倾向
+const savePreference = () => {
+  ElMessage.success('保存成功')
 }
 </script>
 
@@ -154,7 +270,7 @@ const handleFileImport = (file) => {
         <el-row :gutter="20">
           <el-col :span="8">
             <el-form-item label="年级">
-              <el-input v-model="formData.grade" placeholder="请输入年级" />
+              <el-input v-model="formData.grade" placeholder="请输入年级（如大一下）" />
             </el-form-item>
           </el-col>
         </el-row>
@@ -165,7 +281,7 @@ const handleFileImport = (file) => {
                 v-model="formData.userDescription"
                 type="textarea"
                 :rows="3"
-                placeholder="请输入您的选课需求和偏好"
+                placeholder="您可以在此进行自我介绍，帮助大模型更好地了解您"
               />
             </el-form-item>
           </el-col>
@@ -210,25 +326,77 @@ const handleFileImport = (file) => {
           </div>
         </div>
       </template>
-      <el-table :data="courses" style="width: 100%">
-        <el-table-column prop="name" label="课程名称" width="180" />
-        <el-table-column prop="teacher" label="教师" width="120" />
-        <el-table-column prop="time" label="上课时间" width="180" />
-        <el-table-column prop="location" label="上课地点" width="180" />
-        <el-table-column label="操作" width="180">
-          <template #default="scope">
-            <el-button type="primary" size="small" @click="showEditCourseDialog(scope.$index)">
-              编辑
-            </el-button>
-            <el-button type="danger" size="small" @click="deleteCourse(scope.$index)">
-              删除
-            </el-button>
+      <el-collapse v-model="activeNames" class="course-collapse">
+        <el-collapse-item v-for="(course, courseIndex) in courses" :key="courseIndex" :name="courseIndex">
+          <template #title>
+            <div class="collapse-title">
+              <span>{{ course.name }}</span>
+              <div class="button-group">
+                <el-button type="primary" size="small" @click.stop="addClass(courseIndex)">
+                  添加班级
+                </el-button>
+                <el-button type="danger" size="small" @click.stop="deleteCourse(courseIndex)">
+                  删除课程
+                </el-button>
+              </div>
+            </div>
           </template>
-        </el-table-column>
-      </el-table>
+          <el-table :data="course.classes" style="width: 100%">
+            <el-table-column prop="id" label="课程编号" width="100" />
+            <el-table-column prop="teacher" label="教师" />
+            <el-table-column prop="time" label="上课时间" />
+            <el-table-column prop="location" label="上课地点" />
+            <el-table-column label="操作" width="200">
+              <template #default="scope">
+                <el-button type="primary" size="small" @click="showEditCourseDialog(courseIndex, scope.$index)">
+                  编辑
+                </el-button>
+                <el-button type="danger" size="small" @click="deleteClass(courseIndex, scope.$index)">
+                  删除
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-collapse-item>
+      </el-collapse>
+    </el-card>
+
+    <el-card class="preference-card">
+      <template #header>
+        <div class="card-header">
+          <span>选课倾向</span>
+        </div>
+      </template>
+      <div class="preference-content">
+        <div class="credits-input">
+          <el-form-item label="最低学分">
+            <el-input-number 
+              v-model="minCredits" 
+              :min="0" 
+              :max="100"
+              @change="handleMinCreditsChange"
+            />
+          </el-form-item>
+          <el-form-item label="最高学分">
+            <el-input-number 
+              v-model="maxCredits" 
+              :min="0" 
+              :max="100"
+              @change="handleMaxCreditsChange"
+            />
+          </el-form-item>
+        </div>
+        <el-input
+          v-model="coursePreference"
+          type="textarea"
+          :rows="4"
+          placeholder="请描述您的选课倾向，例如：希望课程时间分布均匀，避免连续上课；优先选择教学经验丰富的老师；对课程难度没有特别要求等"
+        />
+      </div>
     </el-card>
 
     <div class="button-container">
+      <el-button type="success" size="large" @click="savePreference">保存</el-button>
       <el-button type="primary" size="large" @click="handleConfirm">开始搜索推荐</el-button>
     </div>
 
@@ -236,16 +404,7 @@ const handleFileImport = (file) => {
     <el-dialog v-model="addCourseDialogVisible" title="添加课程" width="500px">
       <el-form :model="newCourse" label-width="80px">
         <el-form-item label="课程名称">
-          <el-input v-model="newCourse.name" />
-        </el-form-item>
-        <el-form-item label="教师">
-          <el-input v-model="newCourse.teacher" />
-        </el-form-item>
-        <el-form-item label="上课时间">
-          <el-input v-model="newCourse.time" />
-        </el-form-item>
-        <el-form-item label="上课地点">
-          <el-input v-model="newCourse.location" />
+          <el-input v-model="newCourse.name" placeholder="请输入课程名称" />
         </el-form-item>
       </el-form>
       <template #footer>
@@ -260,22 +419,25 @@ const handleFileImport = (file) => {
     <el-dialog v-model="editCourseDialogVisible" title="编辑课程" width="500px">
       <el-form :model="editingCourse" label-width="80px">
         <el-form-item label="课程名称">
-          <el-input v-model="editingCourse.name" />
+          <el-input v-model="editingCourse.name" disabled />
+        </el-form-item>
+        <el-form-item label="课程编号">
+          <el-input v-model.number="editingCourse.classes[0].id" type="number" />
         </el-form-item>
         <el-form-item label="教师">
-          <el-input v-model="editingCourse.teacher" />
+          <el-input v-model="editingCourse.classes[0].teacher" />
         </el-form-item>
         <el-form-item label="上课时间">
-          <el-input v-model="editingCourse.time" />
+          <el-input v-model="editingCourse.classes[0].time" />
         </el-form-item>
         <el-form-item label="上课地点">
-          <el-input v-model="editingCourse.location" />
+          <el-input v-model="editingCourse.classes[0].location" />
         </el-form-item>
       </el-form>
       <template #footer>
         <span class="dialog-footer">
           <el-button @click="editCourseDialogVisible = false">取消</el-button>
-          <el-button type="primary" @click="editCourse">确定</el-button>
+          <el-button type="primary" @click="editClass">确定</el-button>
         </span>
       </template>
     </el-dialog>
@@ -287,28 +449,38 @@ body {
   margin: 0;
   padding: 0;
   background-color: #f5f7fa;
+  min-height: 100vh;
 }
 
 #app {
   width: 100%;
   min-height: 100vh;
   background-color: #f5f7fa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
 
 <style scoped>
 .app-container {
+  width: 90%;
   max-width: 1200px;
+  min-width: 800px;
   margin: 20px auto;
-  padding: 0 20px;
-  min-height: 100vh;
+  padding: 20px;
+  min-height: calc(100vh - 40px);
+  display: flex;
+  flex-direction: column;
 }
 
 .config-card,
-.course-card {
+.course-card,
+.preference-card {
   margin-bottom: 20px;
   border-radius: 8px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  flex: 1;
 }
 
 .card-header {
@@ -323,6 +495,7 @@ body {
 .button-container {
   display: flex;
   justify-content: center;
+  gap: 20px;
   margin-top: 20px;
 }
 
@@ -388,5 +561,63 @@ body {
 .button-group {
   display: flex;
   gap: 10px;
+}
+
+.course-collapse {
+  margin-top: 20px;
+}
+
+:deep(.el-collapse-item__header) {
+  font-size: 16px;
+  font-weight: 600;
+}
+
+:deep(.el-collapse-item__content) {
+  padding: 20px;
+}
+
+.collapse-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding-right: 20px;
+}
+
+.collapse-title .button-group {
+  display: flex;
+  gap: 10px;
+}
+
+:deep(.el-collapse-item__header) {
+  font-size: 16px;
+  font-weight: 600;
+  padding-right: 20px;
+}
+
+.preference-card {
+  margin-bottom: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.preference-content {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.credits-input {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 10px;
+}
+
+.credits-input :deep(.el-form-item) {
+  margin-bottom: 0;
+}
+
+.credits-input :deep(.el-input-number) {
+  width: 120px;
 }
 </style>
