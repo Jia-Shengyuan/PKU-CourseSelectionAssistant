4b16f118815d45a00ac9d6b91fa320ccf357b6d7
06devout <2062593323@qq.com>
2025-05-24 18:20:10 +0800

    修conflict

--- api/models/course.py
+++ api/models/course.py
@@ -5,11 +5,7 @@ class CourseSearchRequest(BaseModel):
     name: str
     class_id: Optional[float] = None
     teacher: Optional[str] = None
-<<<<<<< HEAD
     experimental_class: Optional[bool] = False
-=======
-    accept_advanced_class: Optional[bool] = False
->>>>>>> f128e3ecb2f345f02467819f5a44c139275d66d6
 
 class FetchCourseByPlanRequest(BaseModel):
     semester: str
