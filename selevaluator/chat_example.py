d3ea87744038e3c568c98388d33510742fca4510
Shengyuan Jia <jia_shengyuan@163.com>
2025-06-03 12:57:29 +0800

    try new prompt

--- selevaluator/chat_example.py
+++ selevaluator/chat_example.py
@@ -7,7 +7,7 @@ from rich.markdown import Markdown
 def normal_chat():
 
     logger = Logger()
-    settings = LLM_Settings()
+    settings = LLM_Settings(model_name="Pro/deepseek-ai/DeepSeek-R1")
     llm = LLM(settings, logger)
 
     messages = [{"role": "system", "content": "你是一个AI助手，请回答用户的问题。"}]
@@ -81,8 +81,8 @@ async def async_chat(queries : List[str], display_while_running : bool = False):
         logger.log(Markdown(result))
 
 def main():
-    # normal_chat()
-    asyncio.run(async_chat(["1+2+...+10=?", "1+2+4+...+128=?", "1+1/2+1/4+...=?"]))       
+    normal_chat()
+    # asyncio.run(async_chat(["1+2+...+10=?", "1+2+4+...+128=?", "1+1/2+1/4+...=?"]))       
 
 if __name__ == "__main__":
     main()
--- selevaluator/course_evaluate/agent/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .settings import LLM_Settings
-from .llm import LLM_API
-
-__all__ = ["LLM", "LLM_Settings"]
